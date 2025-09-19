aee2156743bef9348f74da2b3cbac2f778fafb86
apurvasinha1511-cmyk <apurvasinha1511@gmail.com>
2025-09-19 11:23:32 +0530

    Create StaticCode.java

---
+++ StaticCode.java
@@ -0,0 +1,9 @@
+class StaticCode {
+    static{
+        System.out.println("Static block is initialised");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("from Main");
+    }
+}
